c3feec62ae2a84fea9babaeaa8fb205503ed92f5
GopeshMedayil <medayilgopesh@gmail.com>
2017-02-21 23:00:45 +0530

    Added Api for create expense

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,14 +1,15 @@
+
 var db = require('../dbconnection');
 var express = require('express');
 var router = express.Router();
 
 /* GET api listing. */
-router.get('/', function (req, res) {
-    res.send('api works');
-});
+// router.get('/', function (req, res) {
+//     res.send('api works');
+// });
 
-// Get all posts
-router.get('/posts', function (req, res) {
+// Get all Expenses
+router.get('/expenseDetails', function (req, res) {
     // Get posts from the mock api
     // This should ideally be replaced with a service that connects to MongoDB
     db.query("Select * from expenses", function (err, rows) {
@@ -16,6 +17,19 @@ router.get('/posts', function (req, res) {
         res.status(200).json(rows);
     });
 
+
+});
+
+//Create Expense
+router.post('/createExpense', function (req, res) {
+    if (req.body) {
+        db.query('INSERT INTO expenses SET ?', req.body, function (err, result) {
+            if (err) {
+                res.status(500).json({ 'code': 500, 'message': "Cannot insert into database" });
+            }
+            res.status(200).json(result);
+        });
+    }
 
 });
 
